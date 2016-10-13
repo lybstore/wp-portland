1df1fbdf936907348f65da8d9df6a9557e814780
lybstore <thelybstore@gmail.com>
2016-10-13 14:14:41 +0100

    wordpress theme install

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$i=0;
+$i++;
+$cfg['Servers'][$i]['user']          = 'cl28-port-db-2';
+$cfg['Servers'][$i]['password']      = 'H-Gt9!JdM'; // use here your password
+$cfg['Servers'][$i]['auth_type']     = 'config';
+?>
