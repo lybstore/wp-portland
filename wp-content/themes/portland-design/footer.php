a5354457d321f693a2f58d78d373722dd90dca73
lybstore <thelybstore@gmail.com>
2016-10-14 10:28:07 +0100

    asset folder added

--- wp-content/themes/portland-design/footer.php
+++ wp-content/themes/portland-design/footer.php
@@ -4,8 +4,16 @@
 	<!-- Footer Widgets-->
 		<footer	id="site-footer" class="col-md-12">
 			<div class="col-xs-12 col-sm-6 col-md-6 footer-logo">
-				<span><img src="/assets/global/logo.png"></span>
+				<span><img src=""></span>
 			</div>
+
+			<div class="col-xs-12 col-sm-5 col-md-5 footer-social-icons">
+				<a href="https://www.instagram.com/portlanddesign/" target="_blank"><img class="footer-icon-size" ng-src="/assets/global/black-instagram.svg"/></a>
+				<a href="https://twitter.com/PortlandDesign" target="_blank"><img class="footer-icon-size" ng-src="/assets/global/black-twitter.svg"/></a>
+				<a href="https://www.facebook.com/PortlandDesign/" target="_blank"><img class="footer-icon-size" ng-src="/assets/global/black-facebook.svg"/></a>
+				<a href="https://www.linkedin.com/company/portland" target="_blank"><img class="footer-icon-size" ng-src="/assets/global/black-linkedin.svg"/></a>
+			</div>
+
 
 
 			<div class="row">
