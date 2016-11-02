3293d3d9d9d54e10b3f15dd6dd1417ed454e13be
lybstore <thelybstore@gmail.com>
2016-11-02 12:23:15 +0000

    back files

--- portland/controllers/aboutCtrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var app = angular.module('portlandApp');
-
-angular
-.module('portlandApp')
-.controller('aboutCtrl', ['$scope', function($scope){
-	
-}])
--- portland/controllers/contactCtrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var app = angular.module('portlandApp');
-
-angular
-.module('portlandApp')
-.controller('contactCtrl', ['$scope', function($scope){
-	
-}])
